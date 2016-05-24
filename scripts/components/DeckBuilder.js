0adebc45a44cdc62ec445ae8c4e3eba23085fb88
Bruno Tarmann <bruno@tarmann.com.br>
2016-05-24 16:16:49 +0100

    Implement Deck Builder

--- scripts/components/DeckBuilder.js
+++ scripts/components/DeckBuilder.js
@@ -1,14 +1,24 @@
 var React = require('react');
 
+var ReactRouter = require('react-router');
+var History = ReactRouter.History;
+
 var DeckBuilder = React.createClass({
+
+  mixins : [History],
+
+  goToDeck : function(type) {
+    var deckId = this.refs.deckId.value;
+    this.history.pushState(null, `/decks/${deckId}-${type}`);
+  },
 
   render: function(){
     return (
-      <div className="deck-builder">
-        <input type="text" />
-        <button>Long Game</button>
-        <button>Short Game</button>
-      </div>
+      <form className="deck-builder">
+        <input type="text" ref="deckId" />
+        <button onClick={this.goToDeck.bind(null, 'long')}>Long Game</button>
+        <button onClick={this.goToDeck.bind(null, 'long')}>Short Game</button>
+      </form>
     )
   }
 
